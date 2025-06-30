ea47479087dab381e7d4ad5f948a1077d5f052bd
PixelRei <playboi.rei17@gmail.com>
2025-06-30 13:21:20 +0200

    Add files via upload

--- OOP/obj1.php
+++ OOP/obj1.php
@@ -14,6 +14,8 @@
     echo "primo cliente: " . $persona1->getName()."<br> <br>"; //-> viene usato per accedere alle proprietà e metodi di un oggetto
 ?>
     <?php
+    /*per estendere classi di file diversi bisogna prima caricare o includere il file contente la classe
+    con: include_once 'nome_file.php'; */
     class Utente extends Person{
         protected $username;
         public function __construct($name, $username){
@@ -22,6 +24,11 @@
         }
         public function getUsername(){
             return $this->username;
+        }
+        //override
+        //per non estendere metodi o intera classe si usa attributo final
+        final function getName(){
+            return "utente Aldo";
         }
     }
     $user1 = new Utente("Aldo", "ALd079");
