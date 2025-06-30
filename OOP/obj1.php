f385a12cb762324aa29010baee04f335669af419
PixelRei <playboi.rei17@gmail.com>
2025-06-30 11:37:22 +0200

    Add files via upload

--- OOP/obj1.php
+++ OOP/obj1.php
@@ -10,8 +10,20 @@
             return $this->name;
         }
     }
-    $persona1 = new Person("Aldo");
-    echo "primo cliente: " . $persona1->getName(); //-> viene usato per accedere alle proprietà e metodi di un oggetto
-
-
+    $persona1 = new Person("Giampaolino");
+    echo "primo cliente: " . $persona1->getName()."<br> <br>"; //-> viene usato per accedere alle proprietà e metodi di un oggetto
+?>
+    <?php
+    class Utente extends Person{
+        protected $username;
+        public function __construct($name, $username){
+            parent::__construct($name);
+            $this->username = $username;
+        }
+        public function getUsername(){
+            return $this->username;
+        }
+    }
+    $user1 = new Utente("Aldo", "ALd079");
+    echo "username di ".$user1->getName()." è:".$user1->getUsername();
 ?>
