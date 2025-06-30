ca07dc8a633378c4eb220916a2e438c454167a31
PixelRei <playboi.rei17@gmail.com>
2025-06-30 10:37:10 +0200

    Add files via upload

---
+++ OOP/obj1.php
@@ -0,0 +1,16 @@
+<?php
+    class Person{  //classe
+        private $name; //proprietà 
+        public function __construct($name){ //costruttore
+            $this->name = $name; //inizializzazione
+        }
+        
+        public function getName(){
+            return $this->name;
+        }
+    }
+    $persona1 = new Person("Aldo");
+    echo "primo cliente: " . $persona1->getName(); //-> viene usato per accedere alle proprietà e metodi di un oggetto
+
+
+?>
