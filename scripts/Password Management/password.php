fcc87757e951fbbc2a77b80a3547a06e5d89f5c2
PixelRei <playboi.rei17@gmail.com>
2025-07-02 08:46:03 +0200

    Add files via upload

--- scripts/Password Management/password.php
+++ scripts/Password Management/password.php
@@ -1,1 +1,9 @@
-
+<?php
+    $userpassword = "L@MiaPass0rd"; //password inserita dall'utente
+    $criptedPassword = password_hash($userpassword, PASSWORD_DEFAULT); //password criptata da noi, salvata in un database
+    if(password_verify($userpassword, $criptedPassword)){
+        echo "Password inserita correttamente";
+    }else{
+        echo "Password non corretta";
+    }
+?>
