ec010531a95c43382b41e989b5f8408fb0637af8
PixelRei <playboi.rei17@gmail.com>
2025-07-03 10:44:49 +0200

    Add files via upload

--- scripts/ExtractDB.php
+++ scripts/ExtractDB.php
@@ -1,8 +1,8 @@
 <?php
     //connecting the file php to mysql database
     try {
-        $hostname = 'localhost';
-        $dbname = 'cicciogamer';
+        $hostname = '127.0.0.1';
+        $dbname = 'mydatabse';
         $user = 'root';
         $pass = '';
         $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass, array(
@@ -12,7 +12,12 @@
         die();
     }
     //we won't write nothing, instead we want to select elements from the database
-    $sql = 'SELECT username, password FROM user';  //definition of the query SQL
+    $sql = 'SELECT username, password FROM utenti';  //definition of the query SQL
     $stmt = $db->prepare($sql);
     $stmt->execute();   
+    //as we want to print all the elements that we have selected, we must use fetch command
+    //we could also use fetchAll(), which is faster but the consumption of memory is higher, and we'll receive a JSON output file
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo 'Username: '.$row['username'].',  password: '.$row['password'];
+    }
 ?>
